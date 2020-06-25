581050216e0f0e17d9bc3494d1aa79a3c6885d1f
Aleksandr Skoriy <al.skoriy@gmail.com>
2020-06-25 13:22:56 +0300

    replace repo parameter to terragrunt root folder parameter

--- cleanup-state-bucket.py
+++ cleanup-state-bucket.py
@@ -30,12 +30,11 @@ def cleanup_empty_states(args):
 
 def cleanup_orphan_states(args):
     repo_actual_blobs = []
-    start_path = os.path.join(args.repo, 'organization')
-    for root, dirs, files in os.walk(start_path):
+    for root, dirs, files in os.walk(args.root):
         if '.terragrunt-cache' not in root:
             is_tf_directory = filter(lambda f: f.endswith('.hcl'), files)
             if is_tf_directory:
-                tf_directory = root.replace(start_path,'').strip('/')
+                tf_directory = root.replace(args.root,'').strip('/')
                 blob_name = os.path.join(tf_directory, 'default.tfstate')
                 repo_actual_blobs.append(blob_name)
 
@@ -101,7 +100,7 @@ def main():
     parser.add_argument('--dryrun', '-n', action='store_true', help="Don't delete state files, just print it")
     parser.add_argument('--noconfirm', '-y', action='store_true', help="Don't ask confirmation for every object (automatically yes)")
     parser.add_argument('--bucket', '-b', action='store', help='GCP bucket to cleanup', default='platform-tf-admin-prod')
-    parser.add_argument('--repo', '-r', action='store', help='Path to tf-infra-gcp repo', default=os.path.abspath(os.path.join(scriptdir, '.')))
+    parser.add_argument('--root', '-r', action='store', help='Path to terragrunt root folder', default='organization')
     cleanup = parser.add_argument_group(title='Cleanup options')
     cleanup.add_argument('--cleanup-empty', '-e', action='store_true', help='Cleanup empty ("resources": []) state files')
     cleanup.add_argument('--cleanup-orphan', '-o', action='store_true', help='Cleanup orphan (no respecting directory in tf-infra-gcp repo) state files')
